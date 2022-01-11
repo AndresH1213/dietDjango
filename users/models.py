6cbf6b1524944c39e55fa41b0e61f3b6f148383e
Andres Hernandez <edanhebla1213@hotmail.com>
2022-01-10 21:44:07 -0500

    profile model first approach

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    username = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    born_date = models.DateField()
+    height = models.IntegerField()
