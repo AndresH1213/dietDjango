af78ab709407f8cd0bed836647e080cfcce6d983
Andres Hernandez <edanhebla1213@hotmail.com>
2022-02-12 01:03:12 -0500

    profile & profile-form routes

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.homePage, name='home'),
     path('register/', views.registerUser, name='register'),
-    path('login/', views.loginUser, name='login')
+    path('login/', views.loginUser, name='login'),
+    path('profile-information/', views.editProfile, name='profile-information'),
+    path('profile/', views.profile, name='profile')
 ]
